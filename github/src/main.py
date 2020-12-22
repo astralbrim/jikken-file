0026a883325037d911f6a0ac13da30985f9fb34d
butsuli-shine <cheichey.1223@gmail.com>
2020-12-22 23:16:12 +0900

    refactaling main.py

--- github/src/main.py
+++ github/src/main.py
@@ -3,13 +3,20 @@ import matplotlib.pyplot as plt
 from  mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 
-comand = result = ''
+comand = result = '' # Initializing
+
+# Setting figure
 fig = plt.figure()
 ax = fig.gca(projection='3d')
 x = np.arange(100)
 y = np.arange(100)
 x, y = np.meshgrid(x, y)
 z = np.empty((100,100), float)
+ax.set_xlabel('alpha')
+ax.set_ylabel('beta')
+ax.set_zlabel('Recognition accuracy')
+
+# start simrecog
 for xi in range(100):
     for yi in range(100):
         command = './simrecog 18317 {} {}'.format(float(xi), float(yi))
@@ -18,10 +25,15 @@ for xi in range(100):
         result = float(result[-1])
         z[xi][yi] = result
 
-ax.set_xlabel('alpha')
-ax.set_ylabel('beta')
-ax.set_zlabel('Recognition accuracy')
-# ax.plot_surface(x, y, z, rstride=1, cmap='hsv', linewidth=0.3)
+# calculate coordinates from one-dimensional coordinates
+max_index = z.argmax()
+max_index_alpha = int(max_index / 100)
+max_index_beta = max_index % 100
+max_value = np.max(z)
+
+# show 
+print(f"MaxIndex:alpha {max_index_alpha}, beta {max_index_beta}")
+print(f"Maxvalue:{max_value}")
 ax.plot_wireframe(x, y, z, color='red', linewidth=0.3)
 plt.show()
 
